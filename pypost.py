ac9f64b2a1e8c1922ec56d10f1da2ab86afb6d11
Alexander Mohn <pogrmman@gmail.com>
2018-12-23 14:52:30 -0600

    Improved layout a bit

--- pypost.py
+++ pypost.py
@@ -18,11 +18,18 @@ class PyPostGUI:
 
         # Setup layout
         self.frame = ttk.Frame(master, padding=(10,10,10,10))
-        self.frame.grid(row=0, column=0)
+        self.frame.grid(row=0, column=0, sticky=N+S+E+W)
         master.columnconfigure(0, weight=1)
         master.rowconfigure(0, weight=1)
-        self.frame.columnconfigure(0, weight=1)
-        self.frame.rowconfigure(0, weight=1)
+        self.frame.columnconfigure(0, weight=0)
+        self.frame.rowconfigure(0, weight=0)
+        self.frame.columnconfigure(1, weight=1)
+        self.frame.rowconfigure(1, weight=0)
+        self.frame.columnconfigure(2, weight=1)
+        self.frame.rowconfigure(2, weight=0)
+        self.frame.rowconfigure(3, weight=0)
+        self.frame.rowconfigure(4, weight=0)
+        self.frame.rowconfigure(5, weight=1)
 
         # Setup requst type selection
         self.req_label = Label(self.frame, text="Request Type:")
@@ -31,29 +38,29 @@ class PyPostGUI:
         self.req_type_get = Radiobutton(self.frame, text="GET", variable=self.req_type, value="GET", indicatoron=False, command=self.set_get)
         self.req_type_post = Radiobutton(self.frame, text="POST", variable=self.req_type, value="POST", indicatoron=False, command=self.set_post)
         self.req_label.grid(row=0, column=0)
-        self.req_type_get.grid(row=0, column=1)
-        self.req_type_post.grid(row=0, column=2)
+        self.req_type_get.grid(row=0, column=1, sticky=E+W)
+        self.req_type_post.grid(row=0, column=2, sticky=E+W)
 
         # Setup headers entry
         self.headers = StringVar()
         self.headers_label = Label(self.frame, text="Additional Headers:")
         self.headers_entry = Entry(self.frame, textvariable=self.headers)
         self.headers_label.grid(row=1, column=0)
-        self.headers_entry.grid(row=1, column=1, columnspan=2)
+        self.headers_entry.grid(row=1, column=1, columnspan=2, sticky=E+W)
         
         # Setup URL entry
         self.url = StringVar()
         self.url_label = Label(self.frame, text="API URL:")
         self.url_entry = Entry(self.frame, textvariable=self.url)
         self.url_label.grid(row=2, column=0)
-        self.url_entry.grid(row=2, column=1, columnspan=2)
+        self.url_entry.grid(row=2, column=1, columnspan=2, sticky=E+W)
 
         # Setup data entry
         self.data = StringVar()
         self.data_label = Label(self.frame, text="Request Data:")
         self.data_entry = Entry(self.frame, textvariable=self.data)
         self.data_label.grid(row=3, column=0)
-        self.data_entry.grid(row=3, column=1, columnspan=2)
+        self.data_entry.grid(row=3, column=1, columnspan=2, sticky=E+W)
         self.data_label.grid_remove()
         self.data_entry.grid_remove()
 
