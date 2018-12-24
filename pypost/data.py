92e5a20c4aed7d9db135ea7d5584dfc43f9e1e18
Alexander Mohn <pogrmman@gmail.com>
2018-12-23 19:10:33 -0600

    Modified regex slightly to accept ":" and "," as alternatives to "=" and ";", respsectively.

--- pypost/data.py
+++ pypost/data.py
@@ -3,9 +3,10 @@ from re import findall
 
 def process(data):
     # Append semicolon to simplify pattern matching
-    if data[-1] != ";":
+    if data[-1] not in (",",";"):
         data = data + ";"
     # Seperate data items into tuples and build dictionary
-    seperated = findall(r"(.+?)\s{0,1}=\s{0,1}(.+?)\s{0,1};\s{0,1}", data)
+    seperated = findall(r"(.+?)\s{0,1}(=|:)\s{0,1}(.+?)\s{0,1}(;|,)\s{0,1}", data)
+    seperated = [(tup[0], tup[2]) for tup in seperated]
     data = dict(seperated)
     return data
